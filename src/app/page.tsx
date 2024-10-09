6bd5db9b90b69a7c88944823689a31ec1880f3f6
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-10-09 22:10:42 +0300

    Set search params filter by categoies and genres

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import CatSwiper from "@/components/CatSwiper";
+import CategoryGenreFilter from "@/components/CategoryGenreFilter";
 import Header from "@/components/Header";
 import MainSwiper from "@/components/MainSwiper";
 import Link from "next/link";
-import { FaAngleDoubleUp, FaFilm, FaPlus, FaStar } from "react-icons/fa";
+import { FaAngleDoubleUp, FaCheck, FaFilm, FaPhotoVideo, FaPlus, FaStar } from "react-icons/fa";
+import { FaClapperboard } from "react-icons/fa6";
 
 
 export default function Home() {
@@ -60,6 +62,52 @@ export default function Home() {
       <div className="my-[20px] mt-[20px] w-full">
         <CatSwiper />
       </div>
+      <div className="flex justify-center w-full mt-[40px]">
+        <ul className="list-none flex items-center justify-between w-[90%] py-[20px] border-b-[2px] border-[#b8b8b81a]">
+          <li>
+            <Link
+              href={'/all'}
+              className={`${active === 1 ? 'text-white' : 'text-[#ffffffb3]'} hover:text-white flex items-center gap-2 text-sm`}
+            >
+              <i><FaPhotoVideo size={14} /></i>
+              <p>Movies</p>
+            </Link>
+
+          </li>
+          <li>
+            <Link
+              href={'/series'}
+              className={`${active === 2 ? 'text-white' : 'text-[#ffffffb3]'} hover:text-white flex items-center gap-2 text-sm`}
+            >
+              <i><FaFilm size={14} /></i>
+              <p>Series</p>
+            </Link>
+
+          </li>
+          <li>
+            <Link
+              href={'/series'}
+              className={`${active === 3 ? 'text-white' : 'text-[#ffffffb3]'} hover:text-white flex items-center gap-2 text-sm`}
+            >
+              <i><FaCheck size={14} /></i>
+              <p>Original Series</p>
+            </Link>
+
+          </li>
+
+          <li>
+            <Link
+              href={'/genre'}
+              className={`${active === 4 ? 'text-white' : 'text-[#ffffffb3]'} hover:text-white flex items-center gap-2 text-sm`}
+            >
+              <i><FaClapperboard size={14} /></i>
+              <p>Genre</p>
+            </Link>
+
+          </li>
+        </ul>
+      </div>
+      <CategoryGenreFilter />
     </div>
   );
 }
