a4ad818d752700669c15b5f784c616aa59dcf568
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-10-13 14:59:33 +0300

    #

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useRouter } from 'next/navigation'
 import React from 'react'
-import { FaArrowRight, FaArrowLeft } from 'react-icons/fa'
+import { FaArrowRight } from 'react-icons/fa'
 
 const Pagination = ({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
     const page = parseInt(searchParams.page as string) || 1
@@ -40,7 +40,8 @@ const Pagination = ({ searchParams }: { searchParams: { [key: string]: string | 
             {/* Next Page Button */}
             <button
                 onClick={() => handlePagination('next')}
-                className='cssbuttons_io_button bg-[#111010] relative text-white p-[0.35em] pl-[1.2em] rounded-[0.9em] font-semibold text-[17px] border-none leading-[0.05em] flex items-center overflow-hidden h-[2.8em] pr-[3.3em] cursor-pointer'>
+                className='cssbuttons_io_button bg-[#111010] relative text-white p-[0.35em] pl-[1.2em] rounded-[0.9em] font-semibold text-[17px] border-none leading-[0.05em] flex items-center overflow-hidden h-[2.8em] pr-[3.3em] cursor-pointer'
+            >
                 Next Page
                 <div className='icon bg-[#dc1818] ml-[1em] absolute flex items-center justify-center h-[2.2em] w-[2.2em] rounded-[0.7em] shadow-pagination right-[0.3em] transition-all duration-300'>
                     <FaArrowRight />
