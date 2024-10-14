46ad5ed42e496ee160993b5f5a5441aa40f1f0d2
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-10-14 17:31:53 +0300

    set series page

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,13 +1,41 @@
+import Card from '@/components/Card'
 import React from 'react'
 
+
 const Movies = () => {
+    const Movies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
     return (
-        <div className='my-[60px] mx-[45px] text-white'>
-            <div className=''>
-                <h1>Movies</h1>
-                <p>Explosive stunts, intense battles, and adrenaline-pumping thrills. Heros face danger head-on, showcasing their skills in action-packed spectacles that leave audiences on the edge.</p>
-            </div>
-        </div>
+        <>
+            <section className='my-[60px] mx-[45px] text-white'>
+                <div className='flex flex-col gap-[20px]'>
+                    <h1 className='text-[48px] capitalize'>Series</h1>
+                    <p className='text-[18px] text-[#999] w-[60%]'>Explosive stunts, intense battles, and adrenaline-pumping thrills. Heros face danger head-on, showcasing their skills in action-packed spectacles that leave audiences on the edge.</p>
+                </div>
+            </section>
+
+            <section className='border-t-[1px] border-solid border-[#444] my-[20px] mx-[45px] py-[60px]'>
+                <div className='flex flex-wrap items-center justify-center gap-[20px]'>
+                    {
+                        Movies.length > 0
+                            ?
+                            Movies.map(movie => (
+                                <Card
+                                    key={movie}
+                                    movie={movie}
+                                    large={true}
+                                />
+                            ))
+
+                            : (
+                                <h1 className='text-red-500 text-[35px]'>No Series Found</h1>
+                            )
+                    }
+                </div>
+            </section>
+
+
+        </>
     )
 }
 
