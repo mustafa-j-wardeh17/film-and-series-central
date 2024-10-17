ebc07c02898519d6064036fd9a4261978e64a6af
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-10-17 23:44:24 +0300

    transfer data to movies page

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,11 +1,28 @@
 import { Metadata } from 'next';
 import React from 'react'
+import prisma from '../../../lib/prisma';
+import Pagination from '@/components/Pagination';
+import Card from '@/components/Card';
 
 export const metadata: Metadata = {
     title: 'Movies'
 };
-const Movies = async() => {
+const Movies = async ({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
+    const [allData, count] = await prisma.$transaction([
+        prisma.mediaContent.findMany({
+            include: {
+                genre: true,
+                category: true,
+                downloadLink: true,
+                language: true,
+                subtitle: true
+            },
+            skip: ((Number(searchParams.page) || 1) - 1) * 10,
+            take: 10
+        }),
+        prisma.mediaContent.count()
 
+    ])
     return (
         <>
             <section className='my-[60px] mx-[45px] text-white'>
@@ -17,21 +34,25 @@ const Movies = async() => {
 
             <section className='border-t-[1px] border-solid border-[#444] my-[20px] mx-[45px] py-[60px]'>
                 <div className='flex flex-wrap items-center justify-center gap-[20px]'>
-                    {/* {
-                        Movies.length > 0
+                    {
+                        allData.length > 0
                             ?
-                            Movies.map(movie => (
+                            allData.map(movie => (
                                 <Card
-                                    key={movie}
+                                    key={movie.id}
                                     movie={movie}
                                     large={true}
                                 />
                             ))
 
                             : (
-                                <h1 className='text-red-500 text-[35px]'>No Series Found</h1>
+                                <h1 className='text-red-500 text-[35px]'>No Movies Found</h1>
                             )
-                    } */}
+                    }
+                    <Pagination
+                        searchParams={searchParams}
+                        count={count}
+                    />
                 </div>
             </section>
 
