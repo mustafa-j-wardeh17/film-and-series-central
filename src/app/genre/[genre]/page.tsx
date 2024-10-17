8995d527efa43aa990466ea07bd1284bfcf7ba5a
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-10-18 00:03:34 +0300

    transfer data to dynamic genre page

--- src/app/genre/[genre]/page.tsx
+++ src/app/genre/[genre]/page.tsx
@@ -1,5 +1,8 @@
 import { Metadata } from 'next'
 import React from 'react'
+import prisma from '../../../../lib/prisma'
+import Card from '@/components/Card'
+import Pagination from '@/components/Pagination'
 
 export async function generateMetadata(
     { params: { genre } }: { params: { genre: string } }
@@ -10,9 +13,27 @@ export async function generateMetadata(
     }
 }
 
+const page = async ({ params: { genre }, searchParams }: { params: { genre: string }, searchParams: { [key: string]: string | undefined } }) => {
+    const [allData, count] = await prisma.$transaction([
+        prisma.mediaContent.findMany({
+            where: {
+                genre: {
+                    name: genre
+                }
+            },
+            include: {
+                genre: true,
+                category: true,
+                downloadLink: true,
+                language: true,
+                subtitle: true
+            },
+            skip: ((Number(searchParams.page) || 1) - 1) * 10,
+            take: 10
+        }),
+        prisma.mediaContent.count()
 
-const page = ({ params: { genre } }: { params: { genre: string } }) => {
-
+    ])
 
     return (
         <>
@@ -24,21 +45,25 @@ const page = ({ params: { genre } }: { params: { genre: string } }) => {
             </section>
             <section className='border-t-[1px] border-solid border-[#444] my-[20px] mx-[45px] py-[60px]'>
                 <div className='flex flex-wrap items-center justify-center gap-[20px]'>
-                    {/* {
-                        Movies.length > 0
+                    {
+                        allData.length > 0
                             ?
-                            Movies.map(movie => (
+                            allData.map(movie => (
                                 <Card
-                                    key={movie}
+                                    key={movie.id}
                                     movie={movie}
                                     large={true}
                                 />
                             ))
 
                             : (
-                                <h1 className='text-red-500 text-[35px]'>No series Found</h1>
+                                <h1 className='text-red-500 text-[35px]'>No Data For {genre} Found</h1>
                             )
-                    } */}
+                    }
+                    <Pagination
+                        searchParams={searchParams}
+                        count={count}
+                    />
                 </div>
             </section>
         </>
