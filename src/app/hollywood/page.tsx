8dd2c7be976fde933a490c8a26d3ccb2ea56b51e
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-10-17 23:41:59 +0300

    transfer data to hollywood category page

--- src/app/hollywood/page.tsx
+++ src/app/hollywood/page.tsx
@@ -1,28 +1,50 @@
 import { Metadata } from 'next';
 import React from 'react'
+import prisma from '../../../lib/prisma';
+import Card from '@/components/Card';
+import Pagination from '@/components/Pagination';
 
 export const metadata: Metadata = {
     title: 'Hollywood'
 };
-const Hollywood = () => {
+const Hollywood = async ({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
+    const [allData, count] = await prisma.$transaction([
+        prisma.mediaContent.findMany({
+            where: {
+                category: {
+                    name: 'hollywood'
+                }
+            },
+            include: {
+                genre: true,
+                category: true,
+                downloadLink: true,
+                language: true,
+                subtitle: true
+            },
+            skip: ((Number(searchParams.page) || 1) - 1) * 10,
+            take: 10
+        }),
+        prisma.mediaContent.count()
 
+    ])
     return (
         <>
             <section className='my-[60px] mx-[45px] text-white'>
                 <div className='flex flex-col gap-[20px]'>
-                    <h1 className='text-[48px] capitalize'>Series</h1>
+                    <h1 className='text-[48px] capitalize'>Hollywood</h1>
                     <p className='text-[18px] text-[#999] w-full md:w-[60%]'>Explosive stunts, intense battles, and adrenaline-pumping thrills. Heros face danger head-on, showcasing their skills in action-packed spectacles that leave audiences on the edge.</p>
                 </div>
             </section>
 
             <section className='border-t-[1px] border-solid border-[#444] my-[20px] mx-[45px] py-[60px]'>
                 <div className='flex flex-wrap items-center justify-center gap-[20px]'>
-                    {/* {
-                        Movies.length > 0
+                    {
+                        allData.length > 0
                             ?
-                            Movies.map(movie => (
+                            allData.map(movie => (
                                 <Card
-                                    key={movie}
+                                    key={movie.id}
                                     movie={movie}
                                     large={true}
                                 />
@@ -31,7 +53,11 @@ const Hollywood = () => {
                             : (
                                 <h1 className='text-red-500 text-[35px]'>No Movie Found</h1>
                             )
-                    } */}
+                    }
+                    <Pagination
+                        searchParams={searchParams}
+                        count={count}
+                    />
                 </div>
             </section>
 
