a7109370eccd5683dabe4b12e3111d54fd0baa0e
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-10-20 15:59:21 +0300

    card design for phone

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -30,7 +30,7 @@ const Pagination = ({ searchParams, totalPages }: { searchParams: { [key: string
             {/* Previous Page Button */}
             <button
                 onClick={() => handlePagination('previous')}
-                className={` bg-[#111010] relative text-white p-[0.35em] pr-[1.2em] rounded-[0.9em] font-semibold sm:text-[17px] text-[14px] border-none leading-[0.05em]  flex items-center overflow-hidden h-[2.8em] pl-[3.3em]  mr-[1em] ${page === 1 ? 'opacity-50 cursor-not-allowed' : 'cssbuttons_io_button cursor-pointer'}`}
+                className={` bg-[#111010] relative text-white p-[0.35em] pr-[1.2em] rounded-[0.9em] font-semibold sm:text-[17px] text-[14px] border-none leading-[0.05em]  flex items-center overflow-hidden h-[2.8em] pl-[3.3em]  mr-[1em] ${page === 1 ? 'opacity-50 cursor-not-allowed hidden' : 'cssbuttons_io_button cursor-pointer'}`}
                 disabled={page === 1}
             >
                 <div className='icon bg-[#dc1818] mr-[1em] absolute flex items-center justify-center h-[2.2em] w-[2.2em] rounded-[0.7em] shadow-pagination left-[0.3em] transition-all duration-300'>
@@ -42,7 +42,7 @@ const Pagination = ({ searchParams, totalPages }: { searchParams: { [key: string
             {/* Next Page Button */}
             <button
                 onClick={() => handlePagination('next')}
-                className={`bg-[#111010] relative text-white p-[0.35em] pl-[1.2em] rounded-[0.9em] font-semibold sm:text-[17px] text-[14px] border-none leading-[0.05em] flex items-center overflow-hidden h-[2.8em] pr-[3.3em]  ${page === totalPages ? 'opacity-50 cursor-not-allowed' : 'cssbuttons_io_button cursor-pointer'}`}
+                className={`bg-[#111010] relative text-white p-[0.35em] pl-[1.2em] rounded-[0.9em] font-semibold sm:text-[17px] text-[14px] border-none leading-[0.05em] flex items-center overflow-hidden h-[2.8em] pr-[3.3em]  ${page === totalPages ? 'opacity-50 cursor-not-allowed hidden' : 'cssbuttons_io_button cursor-pointer'}`}
                 disabled={page === totalPages}
 
             >
