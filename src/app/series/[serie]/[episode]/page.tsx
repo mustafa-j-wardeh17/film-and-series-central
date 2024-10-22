2f9f0f086cc9f666e2d128ba7e959b57c1efed5b
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-10-22 12:55:44 +0300

    set dynamic [serie] page by fetching it's episodes and the latest series from db

--- src/app/series/[serie]/[episode]/page.tsx
+++ src/app/series/[serie]/[episode]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Episode = async ({ params: { serie, episode }, searchParams }: { params: { serie: string, episode: string }, searchParams: { [key: string]: string | undefined } }) => {
     return (
-        <div>Episode</div>
+        <div>{serie} {episode}</div>
     )
 }
 
---
+++ src/components/SerieCard.tsx
@@ -0,0 +1,77 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { FaEye, FaHeart, FaStar } from 'react-icons/fa'
+
+export interface CatSwiperProps {
+    id: number;
+    title: string;
+    bgposter?: string;
+    slug: string;
+    year?: number;
+    rating?: number;
+    type?: 'serie' | 'media';
+
+}
+interface dataFromSerieProps {
+    episodeNum: number;
+    serieImg: string;
+    serieRating: number;
+    serieYear: number;
+    serieSlug: string;
+}
+const SerieCard = ({ media, dataFromSerie, type }: { dataFromSerie?: dataFromSerieProps, type: 'episode' | 'series', media: CatSwiperProps }) => {
+    return (
+        <div key={media.id} className={`max-w-[200px] min-w-[200px] h-[340px]`}>
+
+            <Link
+                href={
+                    type === 'series'
+                        ? `/series/${media.slug}`
+                        : `/series/${dataFromSerie?.serieSlug}/${media.slug}`
+                }
+            >
+                <div className='w-full hover:scale-[0.99]  hover:shadow-white hover:border-2 hover:border-red-500 relative h-[80%] rounded-[8px] overflow-hidden'>
+                    <Image
+                        src={(type === 'episode' ? dataFromSerie?.serieImg : media.bgposter) || '/img/img.jpg'}
+                        alt={`${media} poster`}
+                        fill
+                    />
+                    {type === 'episode' && (
+                        <div className='absolute z-[60] top-[20px] -left-[10px]  px-5 py-1 rounded-md  bg-black flex items-center justify-center '>
+                            <h3 className='capitalize text-[14px]'>{(dataFromSerie?.episodeNum || 0) + 1}</h3>
+                        </div>
+                    )}
+                </div>
+                <div className='w-full h-[15%] mt-[5px]'>
+                    <h5 className='overflow-ellipsis text-nowrap font-bold overflow-hidden text-[14px]'>{media.title}</h5>
+                    <h6 className='flex items-center justify-between text-[13px] py-[2px]'>
+                        <span className='text-[12px] text-neutral-400'>{
+                            type === 'series'
+                                ? media.year
+                                : dataFromSerie?.serieYear
+                        }</span>
+                        <div className='flex items-center'>
+                            <i className='mr-[7px] mt-[5px] w-[10px] text-[9px] text-[#ffffff80]'>
+                                <FaHeart />
+                            </i>
+                            <i className='mr-[7px] mt-[5px] w-[10px] text-[9px] text-[#ffffff80]'>
+                                <FaEye />
+                            </i>
+                            <i className='mr-[7px] mt-[5px] w-[10px] text-[9px] text-[#ffffff80]'>
+                                <FaStar />
+                            </i>
+                            <h6 className='text-[13px] text-yellow-500 mt-[3px] '>{
+                                type === 'series'
+                                    ? media.rating
+                                    : dataFromSerie?.serieRating
+                            }</h6>
+                        </div>
+                    </h6>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default SerieCard
