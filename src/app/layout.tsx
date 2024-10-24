ef0bbb609852554d34b5e5025924b0e2d726023a
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-10-24 14:11:32 +0300

    @

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,8 @@ export default async function RootLayout({
         {/* Customize the status bar color for Safari on iOS */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="#111010" />
+        <meta name="theme-color" content="#111010" />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
